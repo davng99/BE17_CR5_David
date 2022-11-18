be6ab839d5f5b323bc9e398a6f6a54836f9aa30d
David Nguyen <davidnguyen030499@gmail.com>
2022-11-18 09:25:37 +0100

    first push

---
+++ components/db_connect.php
@@ -0,0 +1,17 @@
+<?php 
+
+$localhost = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "be17_cr5_animal_adoption_david";
+
+// create connection
+$connect = new  mysqli($localhost, $username, $password, $dbname);
+
+// check connection
+if ($connect->connect_error) {
+    die("Connection failed: " . $connect->connect_error);
+}
+//  else {
+//     echo "Successfully Connected";
+// }
